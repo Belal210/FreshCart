bc201484cf1e34a2292a33201bebd906b3c29c17
Belal210 <belalsalem8545@gmail.com>
2025-08-15 17:37:11 +0300

    Update Signup.jsx

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -84,7 +84,7 @@ export default function Signup() {
       <main className="py-12 bg-gray-50">
         <div className="container grid lg:grid-cols-2 lg:gap-12">
           {/* Left side */}
-          <div className="w-full space-y-8 py-10">
+          <div className="w-80 md:w-full space-y-8 py-10">
             <div className="welcome-msg">
               <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold">
                 Welcome to <span className="text-primary-600">FreshCart</span>
@@ -153,9 +153,9 @@ export default function Signup() {
             </div>
           </div>
           {/* Right side (Form) */}
-          <div className="w-full px-5 py-10 space-y-8 bg-white rounded-xl shadow-xl">
+          <div className="w-80 md:w-full px-5 py-10 space-y-8 bg-white rounded-xl shadow-xl">
             <div className="text-center">
-              <h2 className="text-3xl font-semibold">Create Your Account</h2>
+              <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold">Create Your Account</h2>
               <p className="mt-1">Start your fresh journey with us todey</p>
             </div>
 
